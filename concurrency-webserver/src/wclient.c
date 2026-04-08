22087748d30e1294791e46b644b813878360d4bf
Carlos Nunez <80909466+carlosnunez09@users.noreply.github.com>
2026-04-08 19:49:14 -0400

    Refactor HTTP client code for clarity and structure

--- concurrency-webserver/src/wclient.c
+++ concurrency-webserver/src/wclient.c
@@ -1,47 +1,47 @@
 //
-// client.c: A very, very primitive HTTP client.
-// 
-// To run, try: 
-//      client hostname portnumber filename
-//
-// Sends one HTTP request to the specified HTTP server.
-// Prints out the HTTP response.
-//
-// For testing your server, you will want to modify this client.  
-// For example:
-// You may want to make this multi-threaded so that you can 
-// send many requests simultaneously to the server.
-//
-// You may also want to be able to request different URIs; 
-// you may want to get more URIs from the command line 
-// or read the list from a file. 
-//
-// When we test your server, we will be using modifications to this client.
-//
+  // client.c: A very, very primitive HTTP client.
+  //
+  // To run, try:
+  //      client hostname portnumber filename
+  //
+  // Sends one HTTP request to the specified HTTP server.
+  // Prints out the HTTP response.
+  //
+  // For testing your server, you will want to modify this client.
+  // For example:
+  // You may want to make this multi-threaded so that you can
+  // send many requests simultaneously to the server.
+  //
+  // You may also want to be able to request different URIs;
+  // you may want to get more URIs from the command line
+  // or read the list from a file.
+  //
+  // When we test your server, we will be using modifications to this client.
+  //
 
-#include "io_helper.h"
+  #include "io_helper.h"
 
-#define MAXBUF (8192)
+  #define MAXBUF (8192)
 
-//
-// Send an HTTP request for the specified file 
-//
-void client_send(int fd, char *filename) {
-    char buf[MAXBUF];
-    char hostname[MAXBUF];
-    
-    gethostname_or_die(hostname, MAXBUF);
-    
-    /* Form and send the HTTP request */
-    sprintf(buf, "GET %s HTTP/1.1\n", filename);
-    sprintf(buf, "%shost: %s\n\r\n", buf, hostname);
-    write_or_die(fd, buf, strlen(buf));
-}
+  //
+  // Send an HTTP request for the specified file
+  //
+  void client_send(int fd, char *filename) {
+      char buf[MAXBUF];
+      char hostname[MAXBUF];
 
-//
-// Read the HTTP response and print it out
-//
- void client_print(int fd) {
+      gethostname_or_die(hostname, MAXBUF);
+
+      /* Form and send the HTTP request */
+      sprintf(buf, "GET %s HTTP/1.1\n", filename);
+      sprintf(buf, "%shost: %s\n\r\n", buf, hostname);
+      write_or_die(fd, buf, strlen(buf));
+  }
+
+  //
+  // Read the HTTP response and print it out
+  //
+  void client_print(int fd) {
       char buf[MAXBUF];
       int n;
 
@@ -51,36 +51,28 @@ void client_send(int fd, char *filename) {
           n = read_or_die(fd, buf, MAXBUF);
       }
   }
-    
-    // Read and display the HTTP Body 
-    n = readline_or_die(fd, buf, MAXBUF);
-    while (n > 0) {
-	printf("%s", buf);
-	n = readline_or_die(fd, buf, MAXBUF);
-    }
-}
 
-int main(int argc, char *argv[]) {
-    char *host, *filename;
-    int port;
-    int clientfd;
-    
-    if (argc != 4) {
-	fprintf(stderr, "Usage: %s <host> <port> <filename>\n", argv[0]);
-	exit(1);
-    }
-    
-    host = argv[1];
-    port = atoi(argv[2]);
-    filename = argv[3];
-    
-    /* Open a single connection to the specified host and port */
-    clientfd = open_client_fd_or_die(host, port);
-    
-    client_send(clientfd, filename);
-    client_print(clientfd);
-    
-    close_or_die(clientfd);
-    
-    exit(0);
-}
+  int main(int argc, char *argv[]) {
+      char *host, *filename;
+      int port;
+      int clientfd;
+
+      if (argc != 4) {
+          fprintf(stderr, "Usage: %s <host> <port> <filename>\n", argv[0]);
+          exit(1);
+      }
+
+      host = argv[1];
+      port = atoi(argv[2]);
+      filename = argv[3];
+
+      /* Open a single connection to the specified host and port */
+      clientfd = open_client_fd_or_die(host, port);
+
+      client_send(clientfd, filename);
+      client_print(clientfd);
+
+      close_or_die(clientfd);
+
+      exit(0);
+  }
